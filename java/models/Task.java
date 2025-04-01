be8b59b9b9c870787c3793f6466d0935131b5274
Gmrgan <115547191+Gmrgan@users.noreply.github.com>
2025-04-01 09:55:13 +0200

    Create Task.java

---
+++ java/models/Task.java
@@ -0,0 +1,25 @@
+package models;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Task implements Serializable {
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private boolean isCompleted;
+
+    public Task(String title, String description, LocalDate dueDate) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.isCompleted = false;
+    }
+
+    // Getters & Setters
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public LocalDate getDueDate() { return dueDate; }
+    public boolean isCompleted() { return isCompleted; }
+    public void setCompleted(boolean completed) { isCompleted = completed; }
+}
